d2d81bba830de81ef1a317ca7a58892f495670e4
G <mail@grahamirvine.com>
2023-05-11 15:35:17 -0600

    Updated controller and views names for users and friendships to be plural Added index and show pages for users and corresponding routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'users/index'
+  root 'users#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users, only: [:show]
 end
