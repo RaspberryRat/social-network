d7cfd66c4d1aaf3a4cfd8fb5d3dc7bd6ac873d7e
G <mail@grahamirvine.com>
2023-05-13 18:23:17 -0600

    working on friendship tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,21 +5,17 @@
     let(:user2) { FactoryBot.create(:user) }
     let!(:friendship) { Friendship.create(user: user1, friend: user2) }
 
+    #TODO Need to rethink this test
+    # context 'friend request already existis' do
 
-    context 'mutual friend created' do
-      it 'creates a mutual friend record' do
-        friendship.add_mutual_friend
-        mutual_friend = Friendship.find_by(user_id: user2, friend_id: user1)
-        expect(Friendship.all.include?(mutual_friend)).to be(true)
-      end
-    end
-
-    context 'mutual friend not created' do
-      it 'receive error message' do
-        friendship.add_mutual_friend
-        expect(friendship.errors[:friend]).to include('mutual friend already exists')
-      end
-    end
+    #   it 'receive error message' do
+    #     friendship.confirm_friend
+    #     friendship2 = Friendship.new(user: user1, friend: user2)
+    #     debugger
+    #     friendship2.confirm_friend
+    #     expect(friendship2.errors[:friend]).to include('Friend has already been taken')
+    #   end
+    # end
 
     context 'when friend confirms friendship' do
       it 'changes confirmed? to true on mutual friend records' do
@@ -34,21 +30,11 @@
         expect(user_friend_record.confirmed?).to be(true)
         expect(mutual_friend_record.confirmed?).to be(true)
       end
-    end
 
-    context 'when friend denies friendship' do
-      it 'deletes both friendship records' do
-        user_friend_record = Friendship.find_by(
-          user_id: user1, friend_id: user2
-          )
-        mutual_friend_record = Friendship.find_by(
-          user_id: user2, friend_id: user1
-          )
-
-        friendship.deny_friend_request
-
-        expect(Friendship.find_by(id: user_friend_record)).to be(nil)
-        expect(Friendship.find_by(id: mutual_friend_record)).to be(nil)
+      it 'creates a mutual friend record' do
+        friendship.confirm_friend
+        mutual_friend = Friendship.find_by(user_id: user2, friend_id: user1)
+        expect(Friendship.all.include?(mutual_friend)).to be(true)
       end
     end
   end
