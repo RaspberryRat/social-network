764fc156b25cfb881c486b7280ccbc27b3aaa6ef
G <mail@grahamirvine.com>
2023-05-18 11:17:55 -0400

    added post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  validates :title, presence: true, length: { in: 1..75 }
+  validates :content, length: { in: 0..500 }
 end
