2f0708f24dd0a8257b5e727a46c6b31a456be2a3
G <mail@grahamirvine.com>
2023-05-19 16:50:35 -0600

    added more sign up tests

--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -13,4 +13,38 @@ RSpec.describe 'Creating a user', type: :system do
     visit root_path
     expect(page).to have_content('fakeman')
   end
+
+  scenario 'invalid username inputs' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'fakeemail@cool.com'
+    fill_in 'Username', with: 'f'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
+
+    expect(page).to have_content('Username is too short')
+  end
+
+  scenario 'non-matching password inputs' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'fakeemail@cool.com'
+    fill_in 'Username', with: 'f'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'passwrd'
+    click_on 'Sign up'
+
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+
+  scenario 'invalid password inputs' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'fakeemail@cool.com'
+    fill_in 'Username', with: 'f'
+    fill_in 'Password', with: 'pass'
+    fill_in 'Password confirmation', with: 'pass'
+    click_on 'Sign up'
+
+    expect(page).to have_content("Password is too short (minimum is 6 characters)")
+  end
+
 end
