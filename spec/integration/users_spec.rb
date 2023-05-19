258f0c6b5d83ca7d747f015158218b7a1c3dab0a
G <mail@grahamirvine.com>
2023-05-19 17:16:05 -0600

    added tests for loggin in

--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -44,7 +44,46 @@ RSpec.describe 'Creating a user', type: :system do
     fill_in 'Password confirmation', with: 'pass'
     click_on 'Sign up'
 
-    expect(page).to have_content("Password is too short (minimum is 6 characters)")
+    expect(page).to have_content('Password is too short (minimum is 6 characters)')
+  end
+end
+
+RSpec.describe 'Logging in a user', type: :system do
+  before do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'fakeemail@cool.com'
+    fill_in 'Username', with: 'fakeman'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
+
+    visit root_path
+    click_on 'Logout'
   end
 
+  scenario 'valid inputs' do
+    visit user_session_path
+    fill_in 'Email', with: 'fakeemail@cool.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+    expect(page).to have_current_path(root_path)
+  end
+
+  scenario 'invalid email inputs' do
+    visit user_session_path
+    fill_in 'Email', with: 'wrongemail@c.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  scenario 'invalid email inputs' do
+    visit user_session_path
+    fill_in 'Email', with: 'fakeemail@cool.com'
+    fill_in 'Password', with: 'password1'
+    click_on 'Log in'
+
+    expect(page).to have_content('Invalid Email or password.')
+  end
 end
