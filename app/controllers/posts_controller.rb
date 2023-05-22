22cb8b324fa93403e32804722f89ef60c2ee3114
G <mail@grahamirvine.com>
2023-05-22 15:34:47 -0600

    can now stream in add a post to user show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to user_posts_path, notice: "Post was successfully created." }
+        format.turbo_stream
+        # format.html { redirect_to user_posts_path(current_user), notice: "Post was successfully created." }
         # format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
