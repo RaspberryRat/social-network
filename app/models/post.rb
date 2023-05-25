11129424414708fcc7366f9299f89eb055ce8638
G <mail@grahamirvine.com>
2023-05-25 09:19:02 -0600

    changed new post box to be closer to facebook, removed title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
-  validates :title, presence: true, length: { in: 1..75 }
-  validates :content, length: { in: 0..500 }
+  validates :content, presence: true, length: { in: 0..500 }
 
   def self.show_posts(user)
     Post.where(author: user)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_150606) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_151704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_150606) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
-    t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
