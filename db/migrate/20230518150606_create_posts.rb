d10d560054057d79a6b28e1a0991a52522461c1f
G <mail@grahamirvine.com>
2023-05-25 09:21:19 -0600

    Remove add title from the create posts migration
    Delete the remove title from posts migration
    
    This will clean up app deployment.

--- db/migrate/20230518150606_create_posts.rb
+++ db/migrate/20230518150606_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.references :author, null: false, foreign_key: { to_table: :users }
-      t.string :title
       t.string :content
       t.timestamps
     end
