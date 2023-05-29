bd9e724e95dcbb4b538aabd0e0cab7cd500a8adb
G <mail@grahamirvine.com>
2023-05-29 13:48:44 -0600

    added validations tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when no username is provided' do
+    it 'is not valid' do
+      user = User.new(username: nil)
+      expect(user).to_not be_valid
+    end
+  end
 end
