4293bdcca42078b6a1bf727c546d3ec246c569be
G <mail@grahamirvine.com>
2023-05-31 11:38:24 -0600

    added tests for liking comments

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    content { "Sample content" }
+    association :author, factory: :user
+    association :post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   let(:post1) { FactoryBot.create(:post) }
   let(:user1) { FactoryBot.create(:user) }
+  let(:user2) {FactoryBot.create(:user) }
+  let(:user3) {FactoryBot.create(:user) }
+  let(:user4) {FactoryBot.create(:user) }
+  let(:comment1) { FactoryBot.create(:comment) }
 
   context 'when comment is valid' do
     it 'is valid' do
@@ -62,6 +66,66 @@ RSpec.describe Comment, type: :model do
       )
 
       expect(comment).to_not be_valid
+    end
+  end
+
+  context 'when a comment has 0 likes' do
+    it 'returns "Nobody likes this" ' do
+      display = comment1.display_likes
+      expect(display).to eq('Nobody likes this')
+    end
+  end
+
+  context 'when a comment has 1 likes' do
+
+    it 'returns "{name of user} likes this" ' do
+      FactoryBot.create(:like, likeable: comment1, user: user1)
+
+      display = comment1.display_likes
+      expect(display).to eq("#{user1.username} likes this")
+    end
+  end
+
+  context 'when a comment has 2 likes' do
+
+    it 'returns "{name of user1} and {name of user2}  like this" ' do
+      FactoryBot.create(:like, likeable: comment1, user: user1)
+      FactoryBot.create(:like, likeable: comment1, user: user2)
+
+
+      display = comment1.display_likes
+      expect(display).to eq("#{user1.username} and #{user2.username} like this")
+    end
+  end
+
+  context 'when a comment has 3 likes' do
+
+    it 'returns "{name of user1}, {name of user 2}, and {name of user3} like this" ' do
+      FactoryBot.create(:like, likeable: comment1, user: user1)
+      FactoryBot.create(:like, likeable: comment1, user: user2)
+      FactoryBot.create(:like, likeable: comment1, user: user3)
+
+      display = comment1.display_likes
+      expect(display).to eq("#{user1.username}, #{user2.username}, and #{user3.username} like this")
+    end
+  end
+
+  context 'when a comment more than 3 likes' do
+
+    it 'returns "{name of user} and 3 others like this" ' do
+      FactoryBot.create(:like, likeable: comment1, user: user1)
+      FactoryBot.create(:like, likeable: comment1, user: user2)
+      FactoryBot.create(:like, likeable: comment1, user: user3)
+      FactoryBot.create(:like, likeable: comment1, user: user4)
+
+      return_values = [
+                        "#{user1.username} and 3 others like this",
+                        "#{user2.username} and 3 others like this",
+                        "#{user3.username} and 3 others like this",
+                        "#{user4.username} and 3 others like this"
+                      ]
+      display = comment1.display_likes
+      expect(return_values).to include(display)
     end
   end
 end
