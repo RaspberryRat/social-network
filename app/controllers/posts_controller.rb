7f98efb9001d9e7a702ab9957e9c73261b6ddcc7
G <mail@grahamirvine.com>
2023-05-30 20:37:53 -0600

    fixed index posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show]
 
   # GET /posts or /posts.json
   def index
