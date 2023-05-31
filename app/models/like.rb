16a04943e34278dea4bdf4bca28427c6902fcedd
G <mail@grahamirvine.com>
2023-05-31 11:23:30 -0600

    updated duplicate method in Like model to account for different types of likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,9 +3,9 @@ class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true
 
 
-  def self.duplicate?(post, user)
+  def self.duplicate?(likeable, user)
     like = Like.where(user_id: user)
-        .where(post_id: post)
+        .where(likeable_id: likeable)
 
     return true unless like.empty?
 
