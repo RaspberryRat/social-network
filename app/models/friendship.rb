412aa487953f1a51553b13b6d4fa72ab1b790b63
G <mail@grahamirvine.com>
2023-06-01 11:50:34 -0600

    added gravatar support, added support to delete friendships when user deleted

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend, class_name: 'User'
+  belongs_to :friend, class_name: 'User', dependent: :destroy
 
   validates :friend_id, uniqueness: { scope: :user_id }
   validate :not_own_friend
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Gravtastic
+  gravtastic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,6 +11,8 @@ class User < ApplicationRecord
 
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships, source: :friend
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
   has_many :posts, foreign_key: 'author_id', class_name: 'Post', dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
