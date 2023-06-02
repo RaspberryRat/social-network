06107ec159567710fa874bff527947076869e76e
G <mail@grahamirvine.com>
2023-06-02 17:04:39 -0600

    got omniauth facebook authentication working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
   validates :first_name, presence: true, length: { in: 3..20 }
   validates :last_name, presence: true, length: { in: 3..20 }
   validates :email, presence: true, uniqueness: true,
@@ -41,6 +41,27 @@ class User < ApplicationRecord
 
   def fullname
     "#{first_name} #{last_name}"
+  end
+
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = omni_firstname(auth.info.name)
+      user.last_name = omni_lastname(auth.info.name)
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  def self.omni_firstname(name)
+    name.split(' ')[0]
+  end
+
+  def self.omni_lastname(name)
+    name.split(' ')[1]
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_213631) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_220611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_213631) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["first_name"], name: "index_users_on_first_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
