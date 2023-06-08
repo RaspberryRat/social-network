58b0253f85b59973d635bbc12497aeeadd5f5766
G <mail@grahamirvine.com>
2023-06-08 14:45:18 -0600

    updated comment associations to be polymorphic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,12 @@ class Comment < ApplicationRecord
   include Likeable
 
   belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
 
-
   validates :content, length: { in: 1..500 }
-
 
   def self.show_comments(post)
     Comment.where(post: post)
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
-  has_many :comments, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   around_destroy :destroy_associated_post
 
