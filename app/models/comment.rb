1a2212100c5466401d9df749c0299decc6c909c2
G <mail@grahamirvine.com>
2023-06-13 11:32:37 -0600

    have comments working, ready to add comment nesting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  include ActionView::RecordIdentifier
   include Likeable
 
   belongs_to :author, class_name: 'User'
@@ -8,6 +9,20 @@ class Comment < ApplicationRecord
   has_many :liking_users, through: :likes, source: :user
 
   validates :content, length: { in: 1..500 }, presence: true
+
+  after_create_commit do
+    # can broadcast_append_later_to to make a background job
+    broadcast_append_to [commentable, :comments],
+      target: "#{dom_id(commentable)}_comments", locals: { user: author }
+  end
+
+  after_update_commit do
+    broadcast_replace_to self, locals: { user: author }
+  end
+
+  after_destroy_commit do
+    broadcast_remove_to self
+  end
 
   def self.show_comments(post)
     Comment.where(post: post)
